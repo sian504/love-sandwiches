f564d0627e586051d1675c5f51001ac670204f17
sian504 <>
2023-11-06 07:00:52 +0000

    Updated validate_data to convert str to ints

--- run.py
+++ run.py
@@ -22,9 +22,10 @@ def get_sales_data():
     print("Example: 10, 20, 30, 40, 50, 60\n")
 
     data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",") 
+
+    sales_data = data_str.split(",")
     validate_data(sales_data)
+
 
 def validate_data(values):
     """
@@ -34,10 +35,11 @@ def validate_data(values):
 
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
-        )
+            )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
 
